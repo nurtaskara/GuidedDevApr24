b11733129b8dad574d438c50ea80745076d4282f
Nur Taşkara <58294300+nurtaskara@users.noreply.github.com>
2024-04-19 05:40:40 +0300

    Contact age in days DBview demo

---
+++ DevSample/SqlScripts/CreateContactAgeDbView/CreateContactAgeDbView.sql
@@ -0,0 +1,8 @@
+create or alter view UsrVwContactAgeDays
+as
+select Id as UsrId, Name as UsrName, BirthDate as UsrBirthDate,
+datediff(day, BirthDate, getdate()) as UsrAgeDays,
+Id as UsrContactId
+from Contact
+
+
